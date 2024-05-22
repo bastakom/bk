5c38ecda6d7959bf5baec8db60c357a6d9392cf5
BK <philip@bastakompisar.se>
2024-05-22 10:52:12 +0200

    remove opacit

--- app/components/Loading/Loading.tsx
+++ app/components/Loading/Loading.tsx
@@ -79,7 +79,7 @@ const Loading = () => {
       gsap.fromTo(
         textRef.current,
         { opacity: 0 },
-        { opacity: 1, duration: 0.5, ease: 'power2.out' }
+        { opacity: 1, duration: 0, ease: 'power2.out' }
       )
     }
   }, [index])
