f00824d700482d0c2c77b1f1da1c80a887fe1104
BK <philip@bastakompisar.se>
2024-08-15 16:54:34 +0200

    copy

--- components/Tiles.js
+++ components/Tiles.js
@@ -14,18 +14,30 @@ const Tiles = ({ blok }) => {
           triggerOnce: true,
           threshold: 0.1,
         })
-
+        console.log(blok)
         return (
-          <div key={index} className="flex flex-col gap-5" ref={ref}>
-            <span className="flex flex-col gap-2">
-              <h2 className="uppercase font-bold">{item.title}</h2>
-              <h3 className="text-6xl font-bold">
-                {inView && <CountUp end={item.number} duration={2} />}
-                {item.procent ? '%' : null}
-              </h3>
-            </span>
-            <p>{item.content}</p>
-          </div>
+          <React.Fragment key={index}>
+            <div className="flex flex-col gap-5" ref={ref}>
+              <span className="flex flex-col gap-2">
+                <h2 className="uppercase font-bold">{item.title}</h2>
+                <h3 className="text-6xl font-bold">
+                  {inView && <CountUp end={item.number} duration={2} />}
+                  {item.procent ? '%' : null}
+                </h3>
+              </span>
+              <p>{item.content}</p>
+            </div>
+
+            {/* Add a text and title after the first 4 tiles */}
+            {index === 3 && (
+              <div className="col-span-full flex flex-col gap-5">
+                <h2 className="uppercase font-bold text-6xl">
+                  {blok.tiles_title}
+                </h2>
+                <p>{blok.tiles_text}</p>
+              </div>
+            )}
+          </React.Fragment>
         )
       })}
     </div>
