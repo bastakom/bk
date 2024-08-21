45645f50a07d5bbf7b07a50301bef3fdf3d42c30
BK <philip@bastakompisar.se>
2024-08-21 10:03:51 +0200

    cookie declaration

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -44,12 +44,13 @@ export default function RootLayout({
           type="text/javascript"
           strategy="beforeInteractive"
         /> */}
-        <Script
+        {/* <Script
           id="CookieDeclaration"
           src="https://consent.cookiebot.com/6261c3fa-3f04-4b5b-967e-dc48fd9022a4/cd.js"
           type="text/javascript"
           async
-        />
+          strategy="beforeInteractive"
+        /> */}
 
         {/* <Script
           src="https://consent.cookiebot.com/6261c3fa-3f04-4b5b-967e-dc48fd9022a4/cd.js"
