03a20c798d24bee6c4adccf1b7bfa6f00a8e94e7
BK <philip@bastakompisar.se>
2024-08-27 13:34:37 +0200

    GTM

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -10,8 +10,6 @@ const Header = dynamic(() => import('./components/Header'), { ssr: false })
 import '../globals.css'
 import '../font.css'
 import LoadingLogo from './components/Loading/LoadingLogo'
-import Head from 'next/head'
-import Script from 'next/script'
 
 storyblokInit({
   accessToken: 'faVE0ToH7Y41wHZy0uSt3Qtt',
@@ -45,9 +43,27 @@ export default function RootLayout({
             type="text/javascript"
             async
           ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N5M8HVH');</script>
+            `,
+            }}
+          />
         </head>
 
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5M8HVH"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
+
           <ThemeProvider defaultTheme="light" attribute="class">
             <Header locale={lang} />
             <LoadingLogo />
