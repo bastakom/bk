1169048fd15536e9cef93f50e8b3b2f3770e7d5c
BK <philip@bastakompisar.se>
2024-09-06 10:02:43 +0200

    testing mobile nav bugg

--- app/[lang]/components/MobileNavigation.tsx
+++ app/[lang]/components/MobileNavigation.tsx
@@ -37,7 +37,7 @@ const MobileNavigation = ({ props }: any) => {
             return (
               <Link
                 href={link}
-                onClick={() => setIsOpen(false)}
+                onTouchStart={() => setIsOpen(false)}
                 key={item._uid}
                 className="font-secondary text-[28px]"
               >
