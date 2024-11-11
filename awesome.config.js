6b4bfc555b2858642ce5d1aa885da341f9207d64
Philip <anlanderp@gmail.com>
2024-11-11 15:48:19 +0100

    robots

--- awesome.config.js
+++ awesome.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-    siteUrl: 'https://bastakompisar.se/sv',
+    siteUrl: 'https://bastakompisar.se', // Replace with your site URL
     generateRobotsTxt: true, // (optional)
-  }
+    sitemapSize: 7000, // (optional) Limit the number of URLs per sitemap file
+    changefreq: 'daily', // (optional) Default change frequency for all URLs
+    priority: 0.7, // (optional) Default priority for all URLs
+    exclude: ['/admin/*'], // (optional) Exclude specific paths
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'https://bastakompisar.se/sitemap-0.xml', // (optional) Additional sitemaps
+      ],
+    },
+  };
