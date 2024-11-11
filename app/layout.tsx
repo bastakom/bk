527a082c986227f408329ae512d9acd0c36f9c9b
Philip <anlanderp@gmail.com>
2024-11-11 14:44:47 +0100

    Joels hittepå

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,40 @@
-import type { Metadata } from 'next'
-import { storyblokInit, apiPlugin } from '@storyblok/react/rsc'
-import './globals.css'
-import './font.css'
-import StoryblokProvider from '@/components/StoryblokProvider'
-import { ThemeProvider } from './[lang]/components/ThemeProvid/theme-provider'
+import type { Metadata } from "next";
+import { storyblokInit, apiPlugin } from "@storyblok/react/rsc";
+import "./globals.css";
+import "./font.css";
+import StoryblokProvider from "@/components/StoryblokProvider";
+import { ThemeProvider } from "./[lang]/components/ThemeProvid/theme-provider";
+import Head from "next/head";
 
 storyblokInit({
-  accessToken: 'faVE0ToH7Y41wHZy0uSt3Qtt',
+  accessToken: "faVE0ToH7Y41wHZy0uSt3Qtt",
   use: [apiPlugin],
   apiOptions: {
-    region: 'eu',
+    region: "eu",
   },
-})
+});
 
 export const metadata: Metadata = {
-  title: 'Bästa kompisar malmö',
-  description: 'En fullservice reklam byrå',
-}
+  title: "Bästa kompisar malmö",
+  description: "En fullservice reklam byrå",
+};
 
 export default function RootLayout({
   children,
   params: { lang },
 }: Readonly<{
-  children: React.ReactNode
-  params: { lang: string }
+  children: React.ReactNode;
+  params: { lang: string };
 }>) {
   return (
     <StoryblokProvider>
       <html lang={lang}>
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="MVGaWH59KC0hiSCCWnHPFU68sqy2reAmTntOeaK4n-I"
+          />
+        </Head>
         <body>
           <ThemeProvider defaultTheme="light" attribute="class">
             {children}
@@ -35,5 +42,5 @@ export default function RootLayout({
         </body>
       </html>
     </StoryblokProvider>
-  )
+  );
 }
