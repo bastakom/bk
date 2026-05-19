06f78143bc17f3a5f11f115764734fa0eedf8059
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-05-19 16:49:57 +0200

    Update not-found.tsx
    Bug testing, removed header and footer

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
-import Header from "./[lang]/components/Header";
-import Footer from "./[lang]/components/Footer";
 
 const Custom404 = () => {
   return (
     <main>
-      <Header />
       <div className="h-[60vh] flex flex-col justify-center items-center">
         <h1 className="text-[45px]">404 - Sidan finns inte</h1>
         <Link href="/">Gå till startsidan</Link>
       </div>
-      <Footer />
     </main>
   );
 };
