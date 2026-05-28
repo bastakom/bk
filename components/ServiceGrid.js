cdb6c400f353d901dbd51a83eb1f2be149eb82b8
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-05-28 16:07:19 +0200

    Update ServiceGrid.js
    removed bug with service grid

--- components/ServiceGrid.js
+++ components/ServiceGrid.js
@@ -6,7 +6,7 @@ const ServiceGrid = ({ blok }) => {
       className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
       {...storyblokEditable(blok)}
     >
-      {blok.tiles?.map((nestedBlok) => (
+      {blok.serviceTiles?.map((nestedBlok) => (
         <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
     </section>
