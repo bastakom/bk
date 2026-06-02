110143f167c53eb905b5985e88b9451fbe982828
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-06-02 13:38:36 +0200

    Update awesome.config.js
    fixed sitemap 404 error

--- awesome.config.js
+++ awesome.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-    siteUrl: 'https://bastakompisar.se', // Replace with your site URL
-    generateRobotsTxt: true, // (optional)
-    sitemapSize: 7000, // (optional) Limit the number of URLs per sitemap file
-    changefreq: 'daily', // (optional) Default change frequency for all URLs
-    priority: 0.7, // (optional) Default priority for all URLs
-    exclude: ['/admin/*'], // (optional) Exclude specific paths
-    robotsTxtOptions: {
-      additionalSitemaps: [
-        'https://bastakompisar.se/sitemap-0.xml', // (optional) Additional sitemaps
-      ],
-    },
-  };
+
+  siteUrl: 'https://bastakompisar.se',
+
+  generateRobotsTxt: true,
+
+  sitemapSize: 7000,
+
+  changefreq: 'daily',
+
+  priority: 0.7,
+
+  exclude: ['/admin/*'],
+
+}
