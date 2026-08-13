ab57cec76ff449000e97fab3fb7c1ecbc30599c4
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-13 13:11:40 +0200

    Create CaseInfoItem.tsx

---
+++ components/NewCase/blocks/CaseInfoItem.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { storyblokEditable } from "@storyblok/react";
+import { render } from "storyblok-rich-text-react-renderer";
+
+interface CaseInfoItemProps {
+  blok: {
+    [key: string]: any;
+    _uid: string;
+    component: string;
+    title?: string;
+    content?: any;
+  };
+}
+
+const CaseInfoItem = ({ blok }: CaseInfoItemProps) => {
+  return (
+    <article {...storyblokEditable(blok)} className="flex flex-col gap-1">
+      {blok.title && (
+        <h2
+          className="text-[clamp(1.25rem,1.7vw,2.5rem)] font-semibold leading-tight"
+          style={{ color: "#111" }}
+        >
+          {blok.title}
+        </h2>
+      )}
+
+      {blok.content && (
+        <div className="font-light-sofia text-[clamp(1rem,1.2vw,1.875rem)] leading-snug">
+          {render(blok.content)}
+        </div>
+      )}
+    </article>
+  );
+};
+
+export default CaseInfoItem;
