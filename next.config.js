9e0b47f68c48f2e7b8f76a4fd6b2ebfd0f8af9d0
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-20 11:49:20 +0200

    Add multiple redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -23,6 +23,71 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: '/en/home',
+        destination: '/en',
+        permanent: true,
+      },
+      {
+        source: '/home',
+        destination: '/sv',
+        permanent: true,
+      },
+      {
+        source: '/cases',
+        destination: '/sv/cases',
+        permanent: true,
+      },
+      {
+        source: '/nyheter',
+        destination: '/sv/nyheter',
+        permanent: true,
+      },
+      {
+        source: '/marknadsfika',
+        destination: '/sv/marknadsfika',
+        permanent: true,
+      },
+      {
+        source: '/vara-tjanster',
+        destination: '/sv/vara-tjanster',
+        permanent: true,
+      },
+      {
+        source: '/omoss',
+        destination: '/sv/omoss',
+        permanent: true,
+      },
+      {
+        source: '/kontakt',
+        destination: '/sv/kontakt',
+        permanent: true,
+      },
+      {
+        source: '/faq',
+        destination: '/sv/faq',
+        permanent: true,
+      },
+      {
+        source: '/filmproduktion',
+        destination: '/sv/filmproduktion',
+        permanent: true,
+      },
+      {
+        source: '/filmproduction',
+        destination: '/sv/filmproduktion',
+        permanent: true,
+      },
+      {
+        source: '/sv/filmproduction',
+        destination: '/sv/filmproduktion',
+        permanent: true,
+      },
+      {
+        source: '/sv/vara-tjanster/film',
+        destination: '/sv/filmproduktion',
+        permanent: true,
+      },
+      {
         source: '/sv/vara-tjanster/cases',
         destination: '/sv/cases',
         permanent: true,
