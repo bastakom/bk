2256633db4decfb0dcc8b49a6186c8f687f5433d
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-21 11:41:23 +0200

    Create page.tsx

---
+++ app/order/page.tsx
@@ -0,0 +1,13 @@
+import OrderForm from '@/Radio/components/OrderForm'
+
+export const metadata = {
+  title: 'Radiobrief - Bästa Kompisar',
+  robots: {
+    index: false,
+    follow: false,
+  },
+}
+
+export default function OrderPage() {
+  return <OrderForm />
+}
