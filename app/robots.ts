7773ac6be9f5d4c55b4a1259c57307bca7f206ff
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-24 13:12:02 +0200

    Add robots.ts for SEO configuration

---
+++ app/robots.ts
@@ -0,0 +1,15 @@
+import type { MetadataRoute } from 'next'
+
+const siteUrl = 'https://bastakompisar.se'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/api/', '/order/'],
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
+  }
+}
