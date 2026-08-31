047813f664fc8048d452f6b6db6556e6e383d9b7
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-31 12:53:35 +0200

    Refactor VartTeam to include section wrapper
    Wrap Team component in a section with padding.

--- components/Team.js
+++ components/Team.js
@@ -2,7 +2,17 @@ import Team from '@/app/[lang]/components/Team/Team'
 import { storyblokEditable } from '@storyblok/react'
 
 const VartTeam = ({ blok }) => {
-  return <Team {...storyblokEditable(blok)} />
+  return (
+    <section
+      {...storyblokEditable(blok)}
+      style={{
+        paddingLeft: '40px',
+        paddingRight: '40px',
+      }}
+    >
+      <Team />
+    </section>
+  )
 }
 
 export default VartTeam
