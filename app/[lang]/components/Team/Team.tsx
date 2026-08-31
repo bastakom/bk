70f2b55552c280b645d3d89e25bc63bacf228792
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-31 12:50:11 +0200

    Add padding to Team component grid

--- app/[lang]/components/Team/Team.tsx
+++ app/[lang]/components/Team/Team.tsx
@@ -18,7 +18,7 @@ const Team = async () => {
   const res = await getTeam()
   return (
     <div
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5"
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 px-5 lg:px-10"
       id="dinakompisar"
     >
       {res.map((member: any) => (
