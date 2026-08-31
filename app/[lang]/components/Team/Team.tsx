9d1e95b0a1576c663d0ae203806b55282c0c5a00
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-31 12:57:28 +0200

    Remove extra padding from Team component

--- app/[lang]/components/Team/Team.tsx
+++ app/[lang]/components/Team/Team.tsx
@@ -18,7 +18,7 @@ const Team = async () => {
   const res = await getTeam()
   return (
     <div
-      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 px-5 lg:px-10"
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5"
       id="dinakompisar"
     >
       {res.map((member: any) => (
