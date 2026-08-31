140f89645f991c3a7cf490edd3062c242bed1432
BK PRODUCTION <126179457+bastakom@users.noreply.github.com>
2026-08-31 13:43:09 +0200

    Add padding to grid container in page.tsx
    Added padding to the grid container for better spacing.

--- app/[lang]/omoss/page.tsx
+++ app/[lang]/omoss/page.tsx
@@ -223,7 +223,7 @@ const Page = async ({ params }: { params: { lang: string } }) => {
           )}
         </div>
         <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 px-5 lg:px-10"
           id="dinakompisar"
         >
           {res.map((member: any) => (
